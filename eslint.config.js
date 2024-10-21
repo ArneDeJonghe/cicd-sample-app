86c2936c11648a83488acbb7d5f83f7458df9407
Arne De Jonghe <arne.dejonghe@student.hogent.be>
2024-10-21 16:52:40 +0200

    fixing eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
 // eslint.config.js
 export default [
     {
-      files: ["**/*.js"],  // Pas aan als je alleen specifieke bestanden wilt linten
+      files: ["**/*.js"],  // Pas aan indien nodig
       languageOptions: {
-        ecmaVersion: 12,  // Komt overeen met ES2021
-        sourceType: "module"
-      },
-      env: {
-        es6: true,
-        es2021: true,
-        node: true,
-        mocha: true
+        ecmaVersion: 12,
+        sourceType: "module",
+        globals: {
+          // Definieer hier de omgevingsvariabelen (voor 'env')
+          es6: true,
+          node: true,
+          mocha: true
+        }
       },
       rules: {
         "indent": ["error", 2, { "SwitchCase": 1 }],
